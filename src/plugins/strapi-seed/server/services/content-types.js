e12dbd0337cf2abe6f0a6d78b7ec7179a5c36418
Djordje Zubac <djordje41zubac@gmail.com>
2023-08-30 14:02:59 +0200

    Added plugin content types to strapi seed content types

--- src/plugins/strapi-seed/server/services/content-types.js
+++ src/plugins/strapi-seed/server/services/content-types.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 module.exports = {
   getContentTypes: () => {
     const apiList = _.filter(strapi.contentTypes, (ct) =>
-      ct.uid.startsWith("api::")
+      ct.uid.startsWith("api::") || ct.uid.startsWith("plugin::")
     );
     return apiList;
   },
